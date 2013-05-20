93353f6d4606a306a365cbc584bb5873331d06be
Pavel Naydenov <naydenov.p.v@gmail.com>
2013-05-20 22:46:20 +0400

    debug console

--- js/game/utils.js
+++ js/game/utils.js
@@ -10,3 +10,17 @@ Utils = new (function(){
         return dx * dx + dy * dy;
     }
 });
+
+var traceDiv = $('<div style="position: absolute; width: 400px;"></div>');
+var traceDivSwitcher = $('<div style="position: absolute; width: 20px; height: 20px; background: blue;"></div>');
+$('body').append(traceDiv);
+$('body').append(traceDivSwitcher);
+$(traceDiv).on('selectstart dragstart', function(evt){ evt.preventDefault(); return false; });
+$(traceDivSwitcher).click(function(){
+    $(traceDiv).toggle();
+});
+function trace(msg){
+    traceDiv.html(traceDiv.html() + (new Date().toLocaleTimeString()) + "> " + msg.toString() + "<br>");
+}
+trace("Console started");
+$(traceDiv).toggle();
