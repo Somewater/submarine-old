c7d52c174a54fdff759713198acb0d8d16a7d99d
somewater <mktsz@mail.ru>
2013-06-20 21:59:01 +0400

    acceleration

--- haxe/src/com/hellespontus/engine/d2/Entity2D.hx
+++ haxe/src/com/hellespontus/engine/d2/Entity2D.hx
@@ -7,12 +7,27 @@ class Entity2D extends Entity{
     private static inline var Y:Int =   1;
     private static inline var VX:Int =  2;
     private static inline var VY:Int =  3;
+    private static inline var AY:Int =  4;
+    private static inline var AY:Int =  5;
+
+    public var accelerated:Bool;
 
     public function new() {
         super();
+        accelerated = false;
+    }
+
+    public function initialize():Void {
+        values = values.concat([0.0, 0.0, 0.0, 0.0]);
+        if(accelerated)
+            values = values.concat([0.0, 0.0]);
     }
 
     override public function advance(delta:Int):Void {
+        if(accelerated){
+            values[VX] += delta * values[AX];
+            values[VY] += delta * values[AY];
+        }
         values[X] += delta * values[VX];
         values[Y] += delta * values[VY];
     }
@@ -21,4 +36,6 @@ class Entity2D extends Entity{
     public function y():Float return values[Y];
     public function vx():Float return values[VX];
     public function vy():Float return values[VY];
+    public function ax():Float return values[AX];
+    public function ay():Float return values[AY];
 }
--- haxe/src/com/hellespontus/engine/d2/Entity2DRotated.hx
+++ haxe/src/com/hellespontus/engine/d2/Entity2DRotated.hx
@@ -4,17 +4,36 @@ import com.hellespontus.engine.core.Entity;
 class Entity2DRotated extends Entity2D{
 
     private static inline var ANGLE:Int =  4;
-    private static inline var VANGLE:Int =  4;
+    private static inline var VANGLE:Int =  5;
+    private static inline var AANGLE:Int =  6;
 
     public function new() {
         super();
     }
 
-    override public function advance(delta:Int):Void {
-        super.advance(delta);
-        values[ANGLE] += delta * values[VANGLE];
+    override public function initialize():Void {
+        super.initialize();
+        values = values.concat([0.0, 0.0]);
+        if(accelerated)
+            values.push(0.0);
     }
 
-    public function angle():Float return values[ANGLE];
-    public function vangle():Float return values[VANGLE];
+    override public function advance(delta:Int):Void {
+        super.advance(delta);
+        if(this.accelerated)
+            values[VANGLE + ACCELERATION_SHIFT] += delta * values[AANGLE + ACCELERATION_SHIFT];
+        values[ANGLE + ACCELERATION_FIX()] += delta * values[VANGLE + ACCELERATION_FIX()];
+    }
+
+    public function angle():Float return values[ANGLE + ACCELERATION_FIX()];
+    public function vangle():Float return values[VANGLE + ACCELERATION_FIX()];
+    public function vangle():Float return values[AANGLE + ACCELERATION_SHIFT];
+
+    private inline var ACCELERATION_SHIFT:Int = 2;
+    private inline function ACCELERATION_FIX():Int {
+        if(this.accelerated)
+            return ACCELERATION_SHIFT;// +2 позиции занимают значения усктрений
+        else
+            return 0;
+    }
 }
